51af9391c70c76a7350e43c905a13639ef0f646b
Mathenics <Mathenics>
2023-02-17 08:30:18 -0800

    Printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print using the printf function
+ * Return:0 (success)
+ * no warning when compiling with -wall gcc option
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
