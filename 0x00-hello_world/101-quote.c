e3a0b9469fe7da88d01144f2a0982e333f6befd3
Mathenics <Mathenics>
2023-02-18 06:53:11 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that print a line to standard error followed by a new line
- * Return 1 (Success)
+ * Return 1 (error)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar,
-	 	2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar,	2015-10-19\n", 
+		stdout);
 	return (1);
 }
