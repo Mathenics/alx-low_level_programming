82bd4aeac0c83539ca822815db18f72a7c224b1e
Mathenics <Mathenics>
2023-02-21 18:18:59 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
+ * print_alphabet - Produce aphabets
+ * Return: void
  */
 
-int main(0)
+void print_alphabet(void)
+
 {
 	char x;
 
-	for (x = 'a'; x <= 'z')
+	for (x = 'a'; x <= 'z'; x++)
 	_putchar(x);
 	_putchar('\n');
-	return (0);
 }
