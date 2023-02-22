d244dc0b0eb828113689f69e7751e2c2654cbc9a
Mathenics <Mathenics>
2023-02-22 12:45:12 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute value
+ * @x: number to be checked
+ * Return: Absolute value or 0
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+	int abs_val;
+
+	abs_val = x = -1;
+	return (abs_val);
+	}
+	return (x);
+}
