df4944345790a71fc44024ef719316b44bab869c
Mathenics <Mathenics>
2023-02-23 06:45:07 -0800

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers and print out the result.
+ * @x: number to be checked
+ * @y: number to be checked
+ * Return: Result.
+ */
+
+int add(int x, int y)
+
+{
+	return (x + y);
+}
