35f5d89cb14d9f8cc50b39b47583e4e2a770ac23
Mathenics <Mathenics>
2023-02-27 05:24:55 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if character is a digit
+ * @a: Number to be checked
+ * Return: 1 if its a digit, 0 else
+ */
+
+int _isdigit(int a)
+{
+	if (a >= 48 && a <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
