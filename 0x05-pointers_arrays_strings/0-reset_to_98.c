06995a5e812cc66f4bc9ec42da924d58b08b1b63
Mathenics <Mathenics>
2023-02-28 16:50:07 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes pointer to an int and update
+ * the value it points to to 98
+ * @n: an integer input pointer
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
