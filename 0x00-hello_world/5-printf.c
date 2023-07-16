e696f99032132d75619a3eb3895f1ee7e024f8b8
Mathenics <aamjb09@gmail.com>
2023-07-15 17:36:38 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Description: A program that prints a string of code using printf function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
