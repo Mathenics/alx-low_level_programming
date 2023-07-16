cb34315c3a3a020ae26d2975c5764c39f763717c
Mathenics <aamjb09@gmail.com>
2023-07-15 17:17:53 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-/*
- * A program that prints a string of code
+/**
+ * main - Entry point
  *
- * Return (0): always
+ * Description: A program that prints a string of code
+ *
+ * Return: Always 0 (success)
  */
 
-int main()
-	{
-		puts("\"Programming is like building a multilingual puzzle\n");
+int main(void)
 
-		return (0);
-	}
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
