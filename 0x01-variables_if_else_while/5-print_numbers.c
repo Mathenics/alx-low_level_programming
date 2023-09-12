c34a9da63a3b5145c8d3e57390e25272578df0c2
Mathenics <aamjb09@gmail.com>
2023-09-12 02:57:18 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Program that prints single digit base 10 numbers
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+printf("%d", x);
+printf("\n");
+
+return (0);
+}
