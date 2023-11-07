6ae62bfc4ce6a65eb1746624f192af055661d9ac
Mathenics <aamjb09@gmail.com>
2023-11-07 08:11:43 -0800

    print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - check the code
+ * print_triangle - function to print triangle of a given size
+ * @size: parameter to be considered
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int i, j; /*declaration of variables*/
+
+	if (size <= 0) /*statement as per question to enter new line*/
+	{
+	_putchar('\n');
+	}
+	for (i = 1; i <= (size); i++)
+	/*for loop for declaring and incrementing i with size increase*/
+	{
+	for (j = 1; j <= (size); j++)
+	/*for loop for declaring & incrementing j size wise*/
+	{
+	if (j <= (size - i)) /*condition to push # to difference of size to i*/
+	_putchar(' ');
+	else
+	_putchar('#'); /*condition to negate the last condition*/
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
