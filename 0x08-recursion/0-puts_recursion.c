919a0157f0b2a514998b019c8a09d3e27491363b
Mathenics <aamjb09@gmail.com>
2023-11-21 07:22:28 -0800

    put recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include "_putchar.c"
 
+/**
+ * _puts_recursion - print out a recursive string
+ * @s: the parameter to be checked
+ *
+ * Return: void
+ */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
+	if (*s == '\0')
+	{
+	_putchar('\n');
+	return;
+	}
 
-    _putchar(*s);
-    s++;
-    _puts_recursion(s);
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
-
